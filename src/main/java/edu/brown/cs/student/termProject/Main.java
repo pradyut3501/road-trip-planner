273919ebf054c13242f53b9caff452d9945be7e1
Sara Syed <sara_syed@brown.edu>
2021-03-31 23:55:55 -0400

    creating main class and react app

---
+++ src/main/java/edu/brown/cs/student/termProject/Main.java
@@ -0,0 +1,193 @@
+package edu.brown.cs.student.termProject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import spark.ExceptionHandler;
+import spark.ModelAndView;
+
+import spark.QueryParamsMap;
+import spark.Response;
+import spark.Request;
+import spark.Spark;
+import spark.TemplateViewRoute;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import com.google.common.collect.ImmutableMap;
+
+import freemarker.template.Configuration;
+
+import org.json.JSONObject;
+import com.google.gson.Gson;
+
+
+/**
+ * The Main class of our project. This is where execution begins.
+ */
+public final class Main {
+
+  private static final int DEFAULT_PORT = 4567;
+  private static final int TIMER_DELAY = 2000;
+
+  // map of latest checkins: maps user id to their latest checkins
+
+  /**
+   * The initial method called when execution begins.
+   * @param args An array of command line arguments
+   */
+  public static void main(String[] args) {
+    new Main(args).run();
+  }
+
+  private final String[] args;
+
+  /**
+   * main constructor.
+   * @param args args.
+   */
+  private Main(String[] args) {
+    this.args = args;
+  }
+
+  /**
+   *
+   */
+  private void run() {
+    System.out.println("You good");
+
+    // Parse command line arguments
+    OptionParser parser = new OptionParser();
+    parser.accepts("gui");
+    parser.accepts("port").withRequiredArg().ofType(Integer.class)
+        .defaultsTo(DEFAULT_PORT);
+    OptionSet options = parser.parse(args);
+
+    if (options.has("gui")) {
+      runSparkServer((int) options.valueOf("port"));
+    }
+
+  }
+
+  /**
+   * creates free marker egine.
+   * @return free marker engine
+   */
+  private static FreeMarkerEngine createEngine() {
+    Configuration config = new Configuration();
+    File templates = new File("src/main/resources/spark/template/freemarker");
+    try {
+      config.setDirectoryForTemplateLoading(templates);
+    } catch (IOException ioe) {
+      System.out.printf("ERROR: Unable use %s for template loading.%n",
+          templates);
+      System.exit(1);
+    }
+    return new FreeMarkerEngine(config);
+  }
+
+  /**
+   * runs the spark server.
+   * @param port given number
+   */
+  private void runSparkServer(int port) {
+    Spark.port(port);
+    Spark.externalStaticFileLocation("src/main/resources/static");
+
+    Spark.options("/*", (request, response) -> {
+      String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+      if (accessControlRequestHeaders != null) {
+        response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+      }
+
+      String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+
+      if (accessControlRequestMethod != null) {
+        response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+      }
+
+      return "OK";
+    });
+
+    FreeMarkerEngine freeMarker = createEngine();
+
+    // Setup Spark Routes
+    Spark.before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+    Spark.exception(Exception.class, new ExceptionPrinter());
+    // Spark.post("/route", new RouteHandler());
+    // Spark.post("/ways", new WaysHandler());
+    // Spark.post("/nearest", new NearestHandler());
+    // Spark.get("/checkin", new CheckinHandler());
+    // Spark.post("/userdata", new UserDataHandler());
+  }
+
+  /**
+   * Handle requests to the front page of our Stars website.
+   */
+  private static class FrontHandler implements TemplateViewRoute {
+    @Override
+    public ModelAndView handle(Request req, Response res) {
+      Map<String, Object> variables = ImmutableMap.of("title",
+          "Stars: Query the database", "answer", " ");
+      return new ModelAndView(variables, "query.ftl");
+    }
+  }
+
+  /**
+   * Display an error page when an exception occurs in the server.
+   */
+  private static class ExceptionPrinter implements ExceptionHandler {
+    @Override
+    public void handle(Exception e, Request req, Response res) {
+      res.status(500);
+      StringWriter stacktrace = new StringWriter();
+      try (PrintWriter pw = new PrintWriter(stacktrace)) {
+        pw.println("<pre>");
+        e.printStackTrace(pw);
+        pw.println("</pre>");
+      }
+      res.body(stacktrace.toString());
+    }
+  }
+
+
+  /**
+   * Class that handles getting nearest traversable node for maps3 frontend.
+   */
+  // private static class NearestHandler implements spark.Route {
+  //   @Override
+  //   public Object handle(Request request, Response response) throws Exception {
+  //
+  //     JSONObject data = new JSONObject(request.body());
+  //     double lat = data.getDouble("lat");
+  //     double lon = data.getDouble("lon");
+  //     List<String> input = new ArrayList<>();
+  //     input.add("nearest");
+  //     input.add(String.valueOf(lat));
+  //     input.add(String.valueOf(lon));
+  //
+  //     //return order: latitude, longitude
+  //     List<Double> dims = NEARESTCOMMAND.getNearestDim(input);
+  //
+  //     Map<String, Object> variables = ImmutableMap.of("nearest", dims);
+  //     return GSON.toJson(variables);
+  //   }
+  // }
+
+
+}
