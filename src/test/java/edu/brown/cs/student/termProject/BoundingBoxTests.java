3b1940b02c4a614b03b32aa265d5632a25d35187
pradyut3501 <77818347+pradyut3501@users.noreply.github.com>
2021-04-07 16:17:40 -0400

    Created JUnit test suite for the findBoundingBox method

--- src/test/java/edu/brown/cs/student/termProject/BoundingBoxTests.java
+++ src/test/java/edu/brown/cs/student/termProject/BoundingBoxTests.java
@@ -15,6 +15,51 @@ public class BoundingBoxTests {
     assertArrayEquals(BoundingBox.findBoundingBoxBounds(
             new double[]{0, 0}, new double[]{0, 0}),
             new double[]{0, 0, 0, 0}, 0);
+
+    assertArrayEquals(BoundingBox.findBoundingBoxBounds(
+            new double[]{10, 10}, new double[]{10, 10}),
+            new double[]{10, 10, 10, 10}, 0);
+  }
+
+  @Test
+  public void testFindBoundingBoxBoundsVerticalLine() {
+    assertArrayEquals(BoundingBox.findBoundingBoxBounds(
+            new double[]{-10, 5}, new double[]{10, 5}),
+            new double[]{-10, 10, 5, 5}, 0);
+
+    assertArrayEquals(BoundingBox.findBoundingBoxBounds(
+            new double[]{34, 10}, new double[]{33, 10}),
+            new double[]{33, 34, 10, 10}, 0);
+  }
+
+  @Test
+  public void testFindBoundingBoxBoundsHorizontalLine() {
+    assertArrayEquals(BoundingBox.findBoundingBoxBounds(
+            new double[]{-3, 10}, new double[]{-3, 0}),
+            new double[]{-3, -3, 0, 10}, 0);
+
+    assertArrayEquals(BoundingBox.findBoundingBoxBounds(
+            new double[]{0, 5}, new double[]{0, 5.01}),
+            new double[]{0, 0, 5, 5.01}, 0);
+  }
+
+  @Test
+  public void testFindBoundingBoxBoundsAllDifferentCoordinates() {
+    assertArrayEquals(BoundingBox.findBoundingBoxBounds(
+            new double[]{-1, 2}, new double[]{5, 4}),
+            new double[]{-1, 5, 2, 4}, 0);
+
+    assertArrayEquals(BoundingBox.findBoundingBoxBounds(
+            new double[]{-1, 4}, new double[]{5, 2}),
+            new double[]{-1, 5, 2, 4}, 0);
+
+    assertArrayEquals(BoundingBox.findBoundingBoxBounds(
+            new double[]{5, 2}, new double[]{-1, 4}),
+            new double[]{-1, 5, 2, 4}, 0);
+
+    assertArrayEquals(BoundingBox.findBoundingBoxBounds(
+            new double[]{5, 4}, new double[]{-1, 2}),
+            new double[]{-1, 5, 2, 4}, 0);
   }
 
 }
