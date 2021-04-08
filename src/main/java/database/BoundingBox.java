68a117d9d06b82cda0cca7a142d2c666ec6e0e6a
pradyut3501 <77818347+pradyut3501@users.noreply.github.com>
2021-04-08 17:33:44 -0400

    Added shopping to the SQL query, documented BoundingBox class and functions

--- src/main/java/database/BoundingBox.java
+++ src/main/java/database/BoundingBox.java
@@ -3,6 +3,7 @@ package database;
 import attractions.Museum;
 import attractions.Park;
 import attractions.Restaurant;
+import attractions.Shop;
 import graph.AttractionNode;
 
 import java.sql.Connection;
@@ -18,12 +19,23 @@ import java.util.List;
  */
 public class BoundingBox {
 
+  /**
+   * Creates a bounding box around the given starting and end positions,
+   * and returns a list of the AttractionNodes from the yelp-database within
+   * this box that are of the given categories.
+   *
+   * @param coords1 - [lat, lon] of the start position
+   * @param coords2 - [lat, lon] of the end position
+   * @param categories - list of acceptable attraction categories
+   * @return list of attraction nodes of the given categories within a
+   * box around the given positions
+   */
   public static List<AttractionNode> findAttractionsBetween(
           double[] coords1, double[] coords2, List<String> categories) {
 
     double[] boundingBoxBounds = findBoundingBoxBounds(coords1, coords2);
 
-    double[] expandedBoundingBoxBounds = expandBoundingBoxBounds(boundingBoxBounds, 4);
+    double[] expandedBoundingBoxBounds = expandBoundingBoxBounds(boundingBoxBounds, 4.0);
 
     try {
       return findAttractionsWithinBoundingBox(expandedBoundingBoxBounds, categories);
@@ -32,6 +44,13 @@ public class BoundingBox {
     }
   }
 
+  /**
+   * Finds the bounds for a bounding box to be created around the given coordinates.
+   *
+   * @param coords1 - [lat, lon] of the first position
+   * @param coords2 - [lat, lon] of the second position
+   * @return [start lat, end lat, start lon, end lon] of the bounding box
+   */
   public static double[] findBoundingBoxBounds(double[] coords1, double[] coords2) {
     double[] bounds = new double[4];
 
@@ -54,7 +73,14 @@ public class BoundingBox {
     return bounds;
   }
 
-  public static double[] expandBoundingBoxBounds(double[] boundingBoxBounds, int expansionFactor) {
+  /**
+   * Extends the given bounding box expansionFactor units in every direction.
+   *
+   * @param boundingBoxBounds - original bounding box to be expanded
+   * @param expansionFactor - number of units to expand the bounding box by in every direction
+   * @return [start lat, end lat, start lon, end lon] of the expanded bounding box
+   */
+  public static double[] expandBoundingBoxBounds(double[] boundingBoxBounds, double expansionFactor) {
     double[] expandedBoundingBoxBounds = new double[4];
 
     expandedBoundingBoxBounds[0] = boundingBoxBounds[0] - expansionFactor;
@@ -65,6 +91,15 @@ public class BoundingBox {
     return expandedBoundingBoxBounds;
   }
 
+  /**
+   * Returns a list of all the attractions in the yelp database within the given
+   * bounding box that are of the given categories.
+   *
+   * @param boundingBoxBounds - [start lat, end lat, start lon, end lon] of the bounding box
+   * @param categories - list of acceptable attraction categories
+   * @return list of attraction nodes of the given categories within the given bounding box
+   * @throws SQLException - if yelp database cannot be successfully queried
+   */
   public static List<AttractionNode> findAttractionsWithinBoundingBox(
           double[] boundingBoxBounds, List<String> categories) throws SQLException {
     Connection conn = Database.getYelpDatabaseConnection();
@@ -116,6 +151,10 @@ public class BoundingBox {
         attractionsWithinBox.add(nextAttraction);
       } else if (categoriesList.contains("Parks") && categories.contains("Park")) {
         AttractionNode nextAttraction = new Park(
+                id, name, location, coords, price, rating);
+        attractionsWithinBox.add(nextAttraction);
+      } else if (categoriesList.contains("Shopping") && categories.contains("Shop")) {
+        AttractionNode nextAttraction = new Shop(
                 id, name, location, coords, price, rating);
         attractionsWithinBox.add(nextAttraction);
       }
